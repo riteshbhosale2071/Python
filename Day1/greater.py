4575ac194e3e55feb8a2827febd6d5bfdc5a9d86
Ritesh Bhosale <riteshbhosale2071@gmail.com>
2026-04-06 21:16:30 +0530

    add greater no.

---
+++ Day1/greater.py
@@ -0,0 +1,6 @@
+num1=int(input("Enter first number: "))
+num2=int(input("Enter second number: "))
+if(num1>num2):
+    print(f"{num1} is greater than {num2}")
+else:
+    print(f"{num2} is greater than {num1}")
