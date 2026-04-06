d63de751c92bcff2a40b9cb6a649f13056c04f97
Ritesh Bhosale <riteshbhosale2071@gmail.com>
2026-04-06 21:18:13 +0530

    add smaller no.

---
+++ Day1/smaller.py
@@ -0,0 +1,6 @@
+num1=int(input("Enter first number: "))
+num2=int(input("Enter second number: "))
+if(num1>num2):
+    print(f"{num2} is smaller than {num1}")
+else:
+    print(f"{num1} is smaller than {num2}")
