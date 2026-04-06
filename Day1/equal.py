541aec011f8811be444c3a08eee99c0d82c98548
Ritesh Bhosale <riteshbhosale2071@gmail.com>
2026-04-06 21:21:13 +0530

    add equal no.

---
+++ Day1/equal.py
@@ -0,0 +1,6 @@
+num1=int(input("Enter first number: "))
+num2=int(input("Enter second number: "))
+if(num1==num2):
+    print(f"{num1} is equal t0 {num2}")
+else:
+    print(f"{num1} is not equal to {num2}")
