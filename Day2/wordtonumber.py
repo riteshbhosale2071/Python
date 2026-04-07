cd5afb189e442709dd83246f16db5080c61e82a4
Ritesh Bhosale <riteshbhosale2071@gmail.com>
2026-04-07 20:58:39 +0530

    add wordtonumber

---
+++ Day2/wordtonumber.py
@@ -0,0 +1,23 @@
+b1 = input("Enter a word: ")
+if b1 == "zero" or b1 == "Zero":
+    print("0")
+elif b1 == "one" or b1 == "One":
+    print("1")
+elif b1 == "two" or b1 == "Two":
+    print("2")
+elif b1 == "three" or b1 == "Three":
+    print("3")
+elif b1 == "four" or b1 == "Four":
+    print("4")
+elif b1 == "five" or b1 == "Five":
+    print("5")
+elif b1 == "six" or b1 == "Six":
+    print("6")
+elif b1 == "seven" or b1 == "Seven":
+    print("7")
+elif b1 == "eight" or b1 == "Eight":
+    print("8")
+elif b1 == "nine" or b1 == "Nine":
+    print("9")
+else:
+    print("Enter between 0 to 9")
