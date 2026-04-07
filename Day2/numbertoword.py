a01d6d854e58708c1055952b7503eb3067dc8300
Ritesh Bhosale <riteshbhosale2071@gmail.com>
2026-04-07 20:57:47 +0530

    add numbertoword.py

---
+++ Day2/numbertoword.py
@@ -0,0 +1,23 @@
+b1 = int(input("Enter a number: "))
+if b1 == 0:
+    print("Zero")
+elif b1 == 1:
+    print("One")
+elif b1 == 2:
+    print("Two")
+elif b1 == 3:
+    print("Three")
+elif b1 == 4:
+    print("Four")
+elif b1 == 5:
+    print("Five")
+elif b1 == 6:
+    print("Six")
+elif b1 == 7:
+    print("Seven")
+elif b1 == 8:
+    print("Eight")
+elif b1 == 9:
+    print("Nine")
+else:
+    print("Enter between 0 to 9")
