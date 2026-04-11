4797282089958b04c2bd234b4fe2ee257c716a2f
Ritesh Bhosale <riteshbhosale2071@gmail.com>
2026-04-11 18:32:31 +0530

    add sortlist

---
+++ Day6/sortlist.py
@@ -0,0 +1,8 @@
+L=list()
+num=int(input("Enter the amount of values of list: "))
+for i in range(1,num+1):
+    val=int(input(f"Enter value {i}: "))
+    L.append(val)
+print("List before sort: ",L)
+L.sort()
+print("List after sort: ",L)
