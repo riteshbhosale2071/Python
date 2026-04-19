2149a40a6d27770284b2ca869ac47d629e2f5064
Ritesh Bhosale <riteshbhosale2071@gmail.com>
2026-04-19 19:15:11 +0530

    add mixset

---
+++ Day14/mixedset.py
@@ -0,0 +1,8 @@
+Math = {"Ravi", "Amit", "Neha", "Priya"}
+Science = {"Neha", "Priya", "Karan", "Amit"}
+
+print("Math Students:", Math)
+print("Science Students:", Science)
+print("Common Students:", Math & Science)
+print("All Students:", Math | Science)
+print("Only Math:", Math - Science)
