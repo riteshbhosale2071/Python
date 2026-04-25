9d33c0693a007727da170f82ac98fc4411f44d67
Ritesh Bhosale <riteshbhosale2071@gmail.com>
2026-04-25 19:35:03 +0530

    add add3numlambda

---
+++ Day19/add3numlambda.py
@@ -0,0 +1,5 @@
+num1=int(input("Enter first num: "))
+num2=int(input("Enter second num: "))
+num3=int(input("Enter third num: "))
+res=lambda a,b,c:a+b+c
+print(f"Addition of {num1} , {num2} and {num3} is",res(num1,num2,num3))
