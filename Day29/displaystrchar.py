987d75cf93c94868b849d46c72685b9f20375f09
Ritesh Bhosale <riteshbhosale2071@gmail.com>
2026-05-05 11:36:46 +0530

    add displaystrchar

---
+++ Day29/displaystrchar.py
@@ -0,0 +1,7 @@
+def printEveIndexChar(str):
+    for i in range(0, len(str)-1, 2):
+        print("index[",i,"]", str[i] )
+inputStr = "Ritesh"
+print("Orginal String is ", inputStr)
+print("Printing only even index chars")
+printEveIndexChar(inputStr)
