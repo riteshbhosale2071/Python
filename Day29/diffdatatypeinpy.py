f050bb12ead51866785dff16af84166440091418
Ritesh Bhosale <riteshbhosale2071@gmail.com>
2026-05-05 11:29:49 +0530

    add diffdatatypeinpy

---
+++ Day29/diffdatatypeinpy.py
@@ -0,0 +1,8 @@
+a = True
+b = 10
+c = 14.3
+d = "Hello"
+print("The data type of a is :",type(a))
+print("The data type of b is :",type(b))
+print("The data type of c is :",type(c))
+print("The data type of d is :",type(d))
