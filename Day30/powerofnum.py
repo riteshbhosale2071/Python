a7c54285bef455b42380555f673521ac77a5f02b
Ritesh Bhosale <riteshbhosale2071@gmail.com>
2026-05-06 19:57:18 +0530

    add powerofnum

---
+++ Day30/powerofnum.py
@@ -0,0 +1,7 @@
+def power():
+    num = int(input("Enter the num: "))
+    power = int(input("Enter the power of num: "))
+    result = num**power
+    print(f"Power {power} of num {num} is",result)
+
+power()
