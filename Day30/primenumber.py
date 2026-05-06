18392c578534315bbd2387df0f19c29832dcd0c7
Ritesh Bhosale <riteshbhosale2071@gmail.com>
2026-05-06 20:09:38 +0530

    add primenumber

---
+++ Day30/primenumber.py
@@ -0,0 +1,13 @@
+def prime():
+    n = int(input("Enter the number: "))
+    if n <= 1:
+        print("Not a prime number")
+    else:
+        for i in range(2, n):
+            if n % i == 0:
+                print("Not a prime number")
+                break
+        else:
+            print("Prime number")
+            
+prime()
