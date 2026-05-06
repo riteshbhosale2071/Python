a92c2e77524177909284540b9c5a509e411c535a
Ritesh Bhosale <riteshbhosale2071@gmail.com>
2026-05-06 19:53:26 +0530

    add area&circumferenceofcircle

---
+++ Day30/area&circumferenceofcircle.py
@@ -0,0 +1,8 @@
+def circle():
+    radius = int(input("Enter the radius: "))
+    area = 3.14*radius*radius
+    circumference = 2*3.14*radius
+    print("Area of circle is",area)
+    print("Circumference of circle is",circumference)
+
+circle()
