34e28054d08b9cbb7651aee7807d881378d500fe
Ritesh Bhosale <riteshbhosale2071@gmail.com>
2026-05-08 20:21:49 +0530

    add checkyearisleapornot

---
+++ Day32/checkyearisleapornot.py
@@ -0,0 +1,14 @@
+def leap():
+    year = int(input("Enter the year:"))
+    if year % 100 == 0:
+        if year % 400 == 0:
+            print(f"{year} is a leap year")
+        else:
+            print(f"{year} is not a leap year")
+    else:
+        if year %4 == 0:
+            print(f"{year} is a leap year")
+        else:
+            print(f"{year} is not a leap year")
+
+leap()
