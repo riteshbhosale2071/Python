12458e20add6c8f2b322f085bfe9115a409c127e
Ritesh Bhosale <riteshbhosale2071@gmail.com>
2026-05-08 20:28:23 +0530

    add displaydayoofweekfromnum

---
+++ Day32/displaydayoofweekfromnum.py
@@ -0,0 +1,20 @@
+def day():
+    num = int(input("Enter any num from 1-7: "))
+    if num == 1:
+        print("Monday")
+    elif num == 2:
+        print("Tuesday")
+    elif num == 3:
+        print("Wednesday")
+    elif num == 4:
+        print("Thursday")
+    elif num == 5:
+        print("Friday")
+    elif num == 6:
+        print("Saturday")
+    elif num == 7:
+        print("Sunday")
+    else:
+        print("Enter a valid num between 1-7")
+
+day()
