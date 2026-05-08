d20737279b3c5d6544491d0d08e09c4f907f22f6
Ritesh Bhosale <riteshbhosale2071@gmail.com>
2026-05-08 20:29:44 +0530

    add checknumis3digit

---
+++ Day32/checknumis3digit.py
@@ -0,0 +1,9 @@
+def check():
+    num = input("Enter a number: ")
+    size = len(num)
+    if size != 3:
+        print("Length of number is not 3")
+    else:
+        print("Length of number is 3")
+
+check()
