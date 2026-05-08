57c24dee754548d601b224e69fc83763c4c69de6
Ritesh Bhosale <riteshbhosale2071@gmail.com>
2026-05-08 20:15:41 +0530

    add transposematrixusingnestedloop

---
+++ Day32/transposematrixusingnestedloop.py
@@ -0,0 +1,11 @@
+transposed = []
+matrix = [[1, 2, 3, 4], [4, 5, 6, 8]]
+
+for i in range(len(matrix[0])):
+    transposed_row = []
+
+    for row in matrix:
+        transposed_row.append(row[i])
+    transposed.append(transposed_row)
+
+print(transposed)
