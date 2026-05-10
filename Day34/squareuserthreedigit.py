ba58dd440bf0de203284a372d4d82d711ed58a61
Ritesh Bhosale <riteshbhosale2071@gmail.com>
2026-05-10 19:50:24 +0530

    add squareuserthreedigit

---
+++ Day34/squareuserthreedigit.py
@@ -0,0 +1,10 @@
+def square():
+    n1 = int(input("Enter the first num: "))
+    n2 = int(input("Enter the second num: "))
+    n3 = int(input("Enter the third num: "))
+    
+    add = n1**2 + n2**2 + n3**2
+
+    print("Squared addition of all nums is",add)
+
+square()
