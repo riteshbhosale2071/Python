1d1bc627e3d3c4f64be67498bd27eefff259e965
riteshbhosale2071 <riteshbhosale2071@gmail.com>
2026-05-12 20:14:50 +0530

    add printshapeofmatrix

---
+++ Day36/printshapeofmatrix.py
@@ -0,0 +1,13 @@
+def display():
+    matrix = [
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9]
+    ]
+
+    rows = len(matrix)
+    cols = len(matrix[0])
+
+    print("Shape of matrix =", (rows, cols))
+
+display()
