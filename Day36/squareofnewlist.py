b30108ea8595bac6b72f19e1e95d34bc07e9a98f
riteshbhosale2071 <riteshbhosale2071@gmail.com>
2026-05-12 20:02:59 +0530

    add squareofnewlist

---
+++ Day36/squareofnewlist.py
@@ -0,0 +1,11 @@
+def find():
+    old_list = [1, 2, 3, 4, 5]
+    new_list = []
+
+    for i in old_list:
+        new_list.append(i ** 2)
+
+    print("Old List =", old_list)
+    print("New List =", new_list)
+
+find()
