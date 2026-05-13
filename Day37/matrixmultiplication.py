0a61562288716bbfe35e08eddde50b5fe63a2c29
riteshbhosale2071 <riteshbhosale2071@gmail.com>
2026-05-13 19:59:30 +0530

    add matrixmultiplication

---
+++ Day37/matrixmultiplication.py
@@ -0,0 +1,25 @@
+def mul():
+    A = [
+        [1, 2],
+        [3, 4]
+    ]
+
+    B = [
+        [5, 6],
+        [7, 8]
+    ]
+
+    result = [
+        [0, 0],
+        [0, 0]
+    ]
+
+    for i in range(len(A)):
+        for j in range(len(B[0])):
+            for k in range(len(B)):
+                result[i][j] += A[i][k] * B[k][j]
+    print("Result Matrix:")
+    for row in result:
+        print(row)
+
+mul()
