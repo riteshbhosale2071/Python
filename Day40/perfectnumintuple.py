561ab4e940e56e7742965b83c00ceaad993c988d
riteshbhosale2071 <riteshbhosale2071@gmail.com>
2026-05-16 19:29:47 +0530

    add perfectnumintuple

---
+++ Day40/perfectnumintuple.py
@@ -0,0 +1,16 @@
+def find():
+    t = (6, 28, 12, 496, 20)
+
+    print("Perfect Numbers:")
+
+    for num in t:
+        total = 0
+
+        for i in range(1, num):
+            if num % i == 0:
+                total += i
+
+        if total == num:
+            print(num)
+
+find()
