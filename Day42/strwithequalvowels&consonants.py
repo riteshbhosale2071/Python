883a7a3dfa41b2a3c088416447b05298bc2cb4dc
riteshbhosale2071 <riteshbhosale2071@gmail.com>
2026-05-18 19:33:05 +0530

    add strwithequalvowels&consonants

---
+++ Day42/strwithequalvowels&consonants.py
@@ -0,0 +1,23 @@
+def find():
+    t = ("boat", "idea", "moon", "data")
+
+    result = []
+
+    for word in t:
+        vowels = 0
+        consonants = 0
+
+        for ch in word.lower():
+
+            if ch in "aeiou":
+                vowels += 1
+
+            elif ch.isalpha():
+                consonants += 1
+
+        if vowels == consonants:
+            result.append(word)
+
+    print(tuple(result))
+
+find()
