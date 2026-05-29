fd7e7d2e33bf3da6158531929f8fe9039afffd6b
riteshbhosale2071 <riteshbhosale2071@gmail.com>
2026-05-29 20:05:59 +0530

    add small&largeinreverse

---
+++ Day52/small&largeinreverse.py
@@ -0,0 +1,10 @@
+def find():
+    L = [12, 45, 7, 89, 23]
+
+    smallest = min(L)
+    largest = max(L)
+
+    print("Largest Number =", largest)
+    print("Smallest Number =", smallest)
+
+find()
