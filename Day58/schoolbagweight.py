21c990fde33fb1cbab23d1f82e907b72c2e8fb76
riteshbhosale2071 <riteshbhosale2071@gmail.com>
2026-06-04 15:53:39 +0530

    add schoolbagweight

---
+++ Day58/schoolbagweight.py
@@ -0,0 +1,10 @@
+def bag():
+    weight = float(input("Enter school bag weight (kg): "))
+
+    if weight <= 5:
+        print("Bag Weight is Normal")
+
+    else:
+        print("Bag is Too Heavy")
+
+bag()
