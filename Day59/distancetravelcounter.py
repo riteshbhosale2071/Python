b1d8cce192f267c9293a66156495e62d1e51eee7
riteshbhosale2071 <riteshbhosale2071@gmail.com>
2026-06-05 19:27:04 +0530

    add distancetravelcounter

---
+++ Day59/distancetravelcounter.py
@@ -0,0 +1,10 @@
+def distance():
+    speed = float(input("Enter speed (km/hr): "))
+        
+    time = float(input("Enter time (hours): "))
+
+    distance = speed * time
+
+    print("Distance Travelled =", distance, "km")
+
+distance()
