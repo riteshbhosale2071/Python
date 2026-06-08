6875d704a65a87e3ffb0df26bfc01ce7363df7f5
riteshbhosale2071 <riteshbhosale2071@gmail.com>
2026-06-08 19:06:41 +0530

    add juicemixingcalculator

---
+++ Day62/juicemixingcalculator.py
@@ -0,0 +1,10 @@
+def juice():
+    apple = float(input("Enter apple juice (ml): "))
+    
+    orange = float(input("Enter orange juice (ml): "))
+
+    total = apple + orange
+
+    print("Total Mixed Juice =", total, "ml")
+
+juice()
