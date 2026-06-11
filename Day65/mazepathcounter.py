176b8ba13bd47c4731313e1a3306bfae70efa0df
riteshbhosale2071 <riteshbhosale2071@gmail.com>
2026-06-11 21:29:42 +0530

    add mazepathcounter

---
+++ Day65/mazepathcounter.py
@@ -0,0 +1,10 @@
+def mazepath():
+    rows = int(input("Enter number of rows: "))
+    
+    cols = int(input("Enter number of columns: "))
+
+    paths = rows * cols
+
+    print("Total Path Cells =", paths)
+
+mazepath()
