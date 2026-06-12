c06d0f70a12b93b2174d6e901ee33d6f1be429d2
riteshbhosale2071 <riteshbhosale2071@gmail.com>
2026-06-12 15:28:37 +0530

    add morningroutinechecklist

---
+++ Day66/morningroutinechecklist.py
@@ -0,0 +1,15 @@
+def routine():
+    tasks = [
+        "Wake Up",
+        "Brush Teeth",
+        "Take Bath",
+        "Eat Breakfast",
+        "Go to School"
+    ]
+
+    print("Morning Routine Checklist\n")
+
+    for task in tasks:
+        print("[ ]", task)
+
+routine()
