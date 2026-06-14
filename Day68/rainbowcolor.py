e1e3cab883523c8b5ab0934d3a425c8e58694aeb
riteshbhosale2071 <riteshbhosale2071@gmail.com>
2026-06-14 18:13:17 +0530

    add rainbowcolor

---
+++ Day68/rainbowcolor.py
@@ -0,0 +1,17 @@
+def rainbow():
+    colors = [
+        "Violet",
+        "Indigo",
+        "Blue",
+        "Green",
+        "Yellow",
+        "Orange",
+        "Red"
+    ]
+
+    print("Rainbow Colors:")
+
+    for color in colors:
+        print(color)
+
+rainbow()
