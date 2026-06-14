9325c274ff22e81d4d8e87b1d774660aaf2496b0
riteshbhosale2071 <riteshbhosale2071@gmail.com>
2026-06-14 18:00:25 +0530

    add spacedistancecalculator

---
+++ Day68/spacedistancecalculator.py
@@ -0,0 +1,10 @@
+def space():
+    distance1 = float(input("Enter first distance (km): "))
+    
+    distance2 = float(input("Enter second distance (km): "))
+
+    total_distance = distance1 + distance2
+
+    print("Total Space Distance =", total_distance, "km")
+
+space()
