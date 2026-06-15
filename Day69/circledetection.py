d53ed967d7d99a481459e518cf241f20794515b6
riteshbhosale2071 <riteshbhosale2071@gmail.com>
2026-06-15 19:52:21 +0530

    add circledetection

---
+++ Day69/circledetection.py
@@ -0,0 +1,10 @@
+def circle():
+    shape = input("Enter shape name: ").lower()
+
+    if shape == "circle":
+        print("Circle Detected")
+
+    else:
+        print("Not a Circle")
+
+circle()
