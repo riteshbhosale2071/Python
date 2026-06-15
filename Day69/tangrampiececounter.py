5862f551099e8a7169d7240b9e3cdf2f56662b95
riteshbhosale2071 <riteshbhosale2071@gmail.com>
2026-06-15 19:54:41 +0530

    add tangrampiececounter

---
+++ Day69/tangrampiececounter.py
@@ -0,0 +1,13 @@
+def tangram():
+    large_triangles = 2
+    medium_triangles = 1
+    small_triangles = 2
+    square = 1
+    parallelogram = 1
+
+    total = (large_triangles + medium_triangles +
+            small_triangles + square + parallelogram)
+
+    print("Total Tangram Pieces =", total)
+
+tangram()
