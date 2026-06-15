f2034d6add2e134751c8ea1cbf6fc6de10df246d
riteshbhosale2071 <riteshbhosale2071@gmail.com>
2026-06-15 19:45:52 +0530

    add rectangle&squarechecker

---
+++ Day69/rectangle&squarechecker.py
@@ -0,0 +1,12 @@
+def check():
+    length = float(input("Enter length: "))
+    
+    breadth = float(input("Enter breadth: "))
+
+    if length == breadth:
+        print("Square")
+
+    else:
+        print("Rectangle")
+
+check()
