81938ce4ae5c681f0671c710b6eef419512ca5b8
riteshbhosale2071 <riteshbhosale2071@gmail.com>
2026-06-18 10:17:52 +0530

    add missingaddend

---
+++ Day72/missingaddend.py
@@ -0,0 +1,10 @@
+def addend():
+    total = int(input("Enter total sum: "))
+    
+    num = int(input("Enter known addend: "))
+
+    missing = total - num
+
+    print("Missing Addend =", missing)
+
+addend()
