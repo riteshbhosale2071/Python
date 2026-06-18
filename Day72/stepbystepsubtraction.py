95dcf6c00bcb9b1a73a98cdaaf7767db355cccdd
riteshbhosale2071 <riteshbhosale2071@gmail.com>
2026-06-18 10:29:12 +0530

    add stepbystepsubtraction

---
+++ Day72/stepbystepsubtraction.py
@@ -0,0 +1,15 @@
+def find():
+    num1 = int(input("Enter first number: "))
+    num2 = int(input("Enter second number: "))
+
+    print("\nStep 1: Write the numbers")
+    print(num1)
+    print("-", num2)
+
+    print("\nStep 2: Subtract the numbers")
+    difference = num1 - num2
+
+    print("\nStep 3: Result")
+    print("Difference =", difference)
+
+find()
