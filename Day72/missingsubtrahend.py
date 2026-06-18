9cf899fd80bfdede9c5e401909dcac1b406224aa
riteshbhosale2071 <riteshbhosale2071@gmail.com>
2026-06-18 10:32:21 +0530

    add missingsubtrahend

---
+++ Day72/missingsubtrahend.py
@@ -0,0 +1,10 @@
+def subtrahend():
+    minuend = int(input("Enter minuend: "))
+    
+    difference = int(input("Enter difference: "))
+
+    subtrahend = minuend - difference
+
+    print("Missing Subtrahend =", subtrahend)
+
+subtrahend()
