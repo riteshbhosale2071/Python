41aa29d97db5870b905274f792100045660f7a37
riteshbhosale2071 <riteshbhosale2071@gmail.com>
2026-06-18 10:09:11 +0530

    add stepbystepadd

---
+++ Day72/stepbystepadd.py
@@ -0,0 +1,15 @@
+def steps():
+    num1 = int(input("Enter first number: "))
+    num2 = int(input("Enter second number: "))
+
+    print("\nStep 1: Write the numbers")
+    print(num1)
+    print("+", num2)
+
+    print("\nStep 2: Add the numbers")
+    total = num1 + num2
+
+    print("\nStep 3: Result")
+    print("Sum =", total)
+
+steps()
