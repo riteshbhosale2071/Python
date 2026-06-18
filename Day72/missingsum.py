f6ff0d0d6b7255981ab311f0edf52f366721a55a
riteshbhosale2071 <riteshbhosale2071@gmail.com>
2026-06-18 10:20:30 +0530

    add missingsum

---
+++ Day72/missingsum.py
@@ -0,0 +1,10 @@
+def sum():
+    num1 = int(input("Enter first number: "))
+    
+    num2 = int(input("Enter second number: "))
+
+    sum_result = num1 + num2
+
+    print("Sum =", sum_result)
+
+sum()
