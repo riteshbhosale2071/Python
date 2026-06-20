9b5af3054baed329df1b9044f35508d10a35acc6
riteshbhosale2071 <riteshbhosale2071@gmail.com>
2026-06-20 18:48:20 +0530

    add multiplicationarray

---
+++ Day73/multiplicationarray.py
@@ -0,0 +1,9 @@
+def mul():
+    rows = int(input("Enter number of rows: "))
+    
+    cols = int(input("Enter number of columns: "))
+
+    for i in range(rows):
+        print("* " * cols)
+
+mul()
