b67e3b0e04631bf55fb226a2bbde65e79783b901
riteshbhosale2071 <riteshbhosale2071@gmail.com>
2026-06-20 18:50:37 +0530

    add objectsinrows&column

---
+++ Day73/objectsinrows&column.py
@@ -0,0 +1,10 @@
+def count():
+    rows = int(input("Enter number of rows: "))
+    
+    columns = int(input("Enter number of columns: "))
+
+    total_objects = rows * columns
+
+    print("Total Objects =", total_objects)
+
+count()
