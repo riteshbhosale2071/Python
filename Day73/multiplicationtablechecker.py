3912fc478c6d0393a966335c6dc69534a168e99c
riteshbhosale2071 <riteshbhosale2071@gmail.com>
2026-06-20 18:52:49 +0530

    add multiplicationtablechecker

---
+++ Day73/multiplicationtablechecker.py
@@ -0,0 +1,9 @@
+def table():
+    number = int(input("Enter a number: "))
+
+    print("Multiplication Table of", number)
+
+    for i in range(1, 11):
+        print(number, "×", i, "=", number * i)
+
+table()
