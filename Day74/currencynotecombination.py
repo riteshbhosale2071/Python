6a8fe1f62fdb0d656e868d752a5650f7d9522b75
riteshbhosale2071 <riteshbhosale2071@gmail.com>
2026-06-21 18:44:54 +0530

    add currencynotecombination

---
+++ Day74/currencynotecombination.py
@@ -0,0 +1,14 @@
+def currency():
+    amount = int(input("Enter amount: "))
+
+    notes = [500, 200, 100, 50, 20, 10]
+
+    for note in notes:
+        count = amount // note
+
+        if count > 0:
+            print(f"₹{note} Notes = {count}")
+
+        amount %= note
+
+currency()
