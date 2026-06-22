d65a6da35ea367dfdc96e33afb8b0b418aa33bb3
riteshbhosale2071 <riteshbhosale2071@gmail.com>
2026-06-22 19:35:57 +0530

    add convertmixedunits

---
+++ Day75/convertmixedunits.py
@@ -0,0 +1,9 @@
+def units():
+    cm = int(input("Enter centimeters:"))
+    m = int(input("Enter meters:"))
+
+    total = (m * 100) + cm
+
+    print("Total of meters & centimeters is",total)
+
+units()
