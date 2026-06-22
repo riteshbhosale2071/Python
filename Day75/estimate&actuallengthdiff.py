084da571d457e95ce8c50c4264b2d28ae92b1141
riteshbhosale2071 <riteshbhosale2071@gmail.com>
2026-06-22 19:11:02 +0530

    add estimate&actuallengthdiff

---
+++ Day75/estimate&actuallengthdiff.py
@@ -0,0 +1,9 @@
+def diff():
+    actual = int(input("Enter the actual length: "))
+    estimate = int(input("Enter the estimated length: "))
+
+    difference = actual - estimate
+
+    print("Difference between actual & estimated is",difference)
+
+diff()
