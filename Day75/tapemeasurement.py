355e5144befc619e7bed49279d45577b478b48e8
riteshbhosale2071 <riteshbhosale2071@gmail.com>
2026-06-22 20:13:02 +0530

    add tapemeasurement

---
+++ Day75/tapemeasurement.py
@@ -0,0 +1,9 @@
+def tape():
+    start = float(input("Enter starting mark (cm) :"))
+    end = float(input("Enter ending mark (cm) :"))
+
+    length = end - start
+
+    print("Measured Length :",length)
+
+tape()
