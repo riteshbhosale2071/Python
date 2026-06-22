e7fc1779f46d88d6d4f356343758ac10b3b011f7
riteshbhosale2071 <riteshbhosale2071@gmail.com>
2026-06-22 19:51:33 +0530

    add perimetercalculator

---
+++ Day75/perimetercalculator.py
@@ -0,0 +1,9 @@
+def perimeter():
+    length = int(input("Enter the length: "))
+    width = int(input("Enter the width: "))
+
+    perimeter = 2 * (length + width)
+
+    print("Perimeter is",perimeter)
+
+perimeter()
