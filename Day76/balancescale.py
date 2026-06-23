2e776d8a918f39ca511e1d2e6c6c71e1d9123ec2
riteshbhosale2071 <riteshbhosale2071@gmail.com>
2026-06-23 19:52:09 +0530

    add balancescale

---
+++ Day76/balancescale.py
@@ -0,0 +1,11 @@
+def scale():
+    n1 = int(input("Enter the object 1 weight (kg) :"))
+    n2 = int(input("Enter the object 2 weight (kg) :"))
+
+    if(n1>n2):
+        print("Object 1 is heavier")
+
+    else:
+        print("Object 2 is heavier")
+
+scale()
