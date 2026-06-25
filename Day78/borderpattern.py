ac3e49eb3241172ae47e6e1e3a73fe29a5441fda
riteshbhosale2071 <riteshbhosale2071@gmail.com>
2026-06-25 19:48:56 +0530

    add borderpattern

---
+++ Day78/borderpattern.py
@@ -0,0 +1,13 @@
+def border():
+    rows = int(input("Enter number of rows: "))
+    cols = int(input("Enter number of columns: "))
+
+    for i in range(rows):
+        for j in range(cols):
+            if i == 0 or i == rows - 1 or j == 0 or j == cols - 1:
+                print("*", end=" ")
+            else:
+                print(" ", end=" ")
+        print()
+
+border()
