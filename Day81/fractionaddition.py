d23d33aea9ad31d6c6424a575a633169948a19b7
riteshbhosale2071 <riteshbhosale2071@gmail.com>
2026-06-29 19:22:08 +0530

    add fractionaddition

---
+++ Day81/fractionaddition.py
@@ -0,0 +1,21 @@
+def fractionaddition():
+    numerator1 = int(input("Enter numerator of first fraction: "))
+    denominator1 = int(input("Enter denominator of first fraction: "))
+
+    numerator2 = int(input("Enter numerator of second fraction: "))
+    denominator2 = int(input("Enter denominator of second fraction: "))
+
+    numerator = (numerator1 * denominator2) + (numerator2 * denominator1)
+    denominator = denominator1 * denominator2
+
+    hcf = 1
+    for i in range(1, min(numerator, denominator) + 1):
+        if numerator % i == 0 and denominator % i == 0:
+            hcf = i
+
+    numerator //= hcf
+    denominator //= hcf
+
+    print("Sum =", numerator, "/", denominator)
+
+fractionaddition()
