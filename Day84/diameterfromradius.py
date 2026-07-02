8c688ba40d1e0d268db22f3286f4ca7392adcfe8
riteshbhosale2071 <riteshbhosale2071@gmail.com>
2026-07-02 19:35:44 +0530

    add diameterfromradius

---
+++ Day84/diameterfromradius.py
@@ -0,0 +1,11 @@
+def diameter():
+    radius = float(input("Enter the radius of the circle: "))
+
+    diameter = 2 * radius
+
+    print("\nCircle Details")
+    print("-" * 25)
+    print("Radius =", radius)
+    print("Diameter =", diameter)
+
+diameter()
