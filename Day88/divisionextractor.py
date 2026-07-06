be48c66444e5859c953ef90d2573e63c474e8b1b
Ritesh Bhosale <riteshbhosale2071@gmail.com>
2026-07-06 19:39:40 +0530

    add divisionextractor

---
+++ Day88/divisionextractor.py
@@ -0,0 +1,13 @@
+def division_details(dividend, divisor):
+    quotient = dividend // divisor
+    remainder = dividend % divisor
+
+    print("Dividend :", dividend)
+    print("Divisor  :", divisor)
+    print("Quotient :", quotient)
+    print("Remainder:", remainder)
+
+dividend = int(input("Enter dividend: "))
+divisor = int(input("Enter divisor: "))
+
+division_details(dividend, divisor)
