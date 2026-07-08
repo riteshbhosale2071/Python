dbf9ae453ba6a527254032039db16647ddedaf91
Ritesh Bhosale <riteshbhosale2071@gmail.com>
2026-07-08 18:53:03 +0530

    add distancebetweencities

---
+++ Day90/.py
@@ -0,0 +1,10 @@
+def distance():
+    city1 = input("Enter first city name: ")
+    city2 = input("Enter second city name: ")
+    distance = float(input("Enter distance between the cities (km): "))
+
+    print("City 1:", city1)
+    print("City 2:", city2)
+    print("Distance:", distance, "km")
+
+distance()
