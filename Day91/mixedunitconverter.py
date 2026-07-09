484dfa2e85ec5c8b17ce532dbb16fb10faa53cb6
Ritesh Bhosale <riteshbhosale2071@gmail.com>
2026-07-09 22:50:34 +0530

    add mixedunitconverter

---
+++ Day91/mixedunitconverter.py
@@ -0,0 +1,9 @@
+def mixedunit():
+    centimetre = int(input("Enter centimetres: "))
+    millimetre = int(input("Enter millimetres: "))
+
+    total_mm = (centimetre * 10) + millimetre
+
+    print("Total Length:", total_mm, "mm")
+
+mixedunit()
