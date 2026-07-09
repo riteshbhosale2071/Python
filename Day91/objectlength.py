b44f8b080f0c9b904d8720422026eaf2ae1f3e19
Ritesh Bhosale <riteshbhosale2071@gmail.com>
2026-07-09 22:56:41 +0530

    add objectlength

---
+++ Day91/objectlength.py
@@ -0,0 +1,8 @@
+def objectlength():
+    object_name = input("Enter object name: ")
+    length = float(input("Enter object length (cm): "))
+
+    print("\nObject Name:", object_name)
+    print("Length:", length, "cm")
+
+objectlength()
