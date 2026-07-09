ae3681118435e3cfda0c6e853be4a54a13dee473
Ritesh Bhosale <riteshbhosale2071@gmail.com>
2026-07-09 22:46:54 +0530

    add mmtocm

---
+++ Day91/mmtocm.py
@@ -0,0 +1,8 @@
+def mmtocm():
+    millimetre = float(input("Enter length in millimetres: "))
+
+    centimetre = millimetre / 10
+
+    print("Length in Centimetres:", centimetre, "cm")
+
+mmtocm()
