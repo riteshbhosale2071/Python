94c7cd8edb2e5db7b69522100ef16667167760e6
Ritesh Bhosale <riteshbhosale2071@gmail.com>
2026-07-10 19:30:57 +0530

    add distancesubtraction

---
+++ Day92/distancesubtraction.py
@@ -0,0 +1,9 @@
+def distancesubtraction():
+    distance1 = float(input("Enter first distance (km): "))
+    distance2 = float(input("Enter second distance (km): "))
+
+    difference = distance1 - distance2
+
+    print("Distance Difference:", difference, "km")
+
+distancesubtraction()
