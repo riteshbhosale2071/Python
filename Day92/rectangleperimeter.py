351aa38c04d50e60f569eae1eb9224ba66a42ab5
Ritesh Bhosale <riteshbhosale2071@gmail.com>
2026-07-10 19:41:29 +0530

    add rectangleperimeter

---
+++ Day92/rectangleperimeter.py
@@ -0,0 +1,9 @@
+def perimeter():
+    length = float(input("Enter length: "))
+    width = float(input("Enter width: "))
+
+    perimeter = 2 * (length + width)
+
+    print("Perimeter of Rectangle:", perimeter)
+
+perimeter()
