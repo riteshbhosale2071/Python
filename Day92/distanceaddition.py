c992b73374a44f13d787479be21cc3a644e8ee5c
Ritesh Bhosale <riteshbhosale2071@gmail.com>
2026-07-10 19:28:40 +0530

    add distanceaddition

---
+++ Day92/distanceaddition.py
@@ -0,0 +1,9 @@
+def distanceaddition():
+    distance1 = float(input("Enter first distance (km): "))
+    distance2 = float(input("Enter second distance (km): "))
+
+    total = distance1 + distance2
+
+    print("Total Distance:", total, "km")
+
+distanceaddition()
