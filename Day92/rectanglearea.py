b0fee8ba4902f95c34578f10aa1496574431a135
Ritesh Bhosale <riteshbhosale2071@gmail.com>
2026-07-10 19:46:24 +0530

    add rectanglearea

---
+++ Day92/rectanglearea.py
@@ -0,0 +1,9 @@
+def rectanglearea():
+    length = float(input("Enter length: "))
+    width = float(input("Enter width: "))
+
+    area = length * width
+
+    print("Area of Rectangle:", area)
+
+rectanglearea()
