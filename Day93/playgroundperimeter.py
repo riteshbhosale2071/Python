8e54abc4a0a0b24648b29f3da418f260e0a05be0
Ritesh Bhosale <riteshbhosale2071@gmail.com>
2026-07-11 19:39:28 +0530

    add playgroundperimeter

---
+++ Day93/playgroundperimeter.py
@@ -0,0 +1,9 @@
+def playgroundperimeter():
+    length = float(input("Enter playground length: "))
+    width = float(input("Enter playground width: "))
+
+    perimeter = 2 * (length + width)
+
+    print("Playground Perimeter:", perimeter)
+
+playgroundperimeter()
