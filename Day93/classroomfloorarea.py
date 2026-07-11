442444a7c4a292a3e8f279ee00ed4ae7466a730b
Ritesh Bhosale <riteshbhosale2071@gmail.com>
2026-07-11 19:36:56 +0530

    add classroomfloorarea

---
+++ Day93/classroomfloorarea.py
@@ -0,0 +1,9 @@
+def classroomfloor():
+    length = float(input("Enter classroom length: "))
+    width = float(input("Enter classroom width: "))
+
+    area = length * width
+
+    print("Classroom Floor Area:", area, "square units")
+
+classroomfloor()
