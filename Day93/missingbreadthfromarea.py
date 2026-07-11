a5700080b25657f74619f782bf00ccac6eecdaef
Ritesh Bhosale <riteshbhosale2071@gmail.com>
2026-07-11 19:26:42 +0530

    add missingbreadthfromarea

---
+++ Day93/missingbreadthfromarea.py
@@ -0,0 +1,9 @@
+def missingbreadth():
+    area = float(input("Enter area of rectangle: "))
+    length = float(input("Enter length: "))
+
+    breadth = area / length
+
+    print("Missing Breadth:", breadth)
+
+missingbreadth()
