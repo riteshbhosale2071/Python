b7b42fae163c8e293f4250ef7b2df061a2c26b6b
Ritesh Bhosale <riteshbhosale2071@gmail.com>
2026-07-12 18:36:38 +0530

    add pictographreader

---
+++ Day94/pictographreader.py
@@ -0,0 +1,9 @@
+def pictographreader():
+    symbol = int(input("Enter value of one symbol: "))
+    symbols = int(input("Enter number of symbols: "))
+
+    total = symbol * symbols
+
+    print("Total Value:", total)
+
+pictographreader()
