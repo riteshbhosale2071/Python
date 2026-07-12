7d1e85d5f48be49885a89d496fac67ebe368cc2b
Ritesh Bhosale <riteshbhosale2071@gmail.com>
2026-07-12 18:58:53 +0530

    add totalobjectsofpictograph

---
+++ Day94/totalobjectsofpictograph.py
@@ -0,0 +1,9 @@
+def totalobjects():
+    item1 = int(input("Enter number of first objects: "))
+    item2 = int(input("Enter number of second objects: "))
+
+    total = item1 + item2
+
+    print("Total Objects:", total)
+
+totalobjects()
