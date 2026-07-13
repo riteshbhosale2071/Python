47905c7b57d107a5d6ae1d7e8e08a6458c1c83d4
Ritesh Bhosale <riteshbhosale2071@gmail.com>
2026-07-13 17:48:09 +0530

    add growingdotpattern

---
+++ Day95/growingdotpattern.py
@@ -0,0 +1,9 @@
+def growingdot():
+    rows = int(input("Enter number of rows: "))
+
+    for i in range(1, rows + 1):
+        for j in range(i):
+            print("*", end=" ")
+        print()
+
+growingdot()
