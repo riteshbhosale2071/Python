3a511a44927c44986f566fbdb338be22953635be
Ritesh Bhosale <riteshbhosale2071@gmail.com>
2026-07-13 17:50:33 +0530

    add shrinkingdotpattern

---
+++ Day95/shrinkingdotpattern.py
@@ -0,0 +1,9 @@
+def shrinkingdot():
+    rows = int(input("Enter number of rows: "))
+
+    for i in range(rows, 0, -1):
+        for j in range(i):
+            print("*", end=" ")
+        print()
+
+shrinkingdot()
