78b58bba5ca4b4c57a139c216f9c35ffeb9ec940
Ritesh Bhosale <riteshbhosale2071@gmail.com>
2026-07-15 19:33:35 +0530

    add mutlistepsub

---
+++ Day97/mutlistepsub.py
@@ -0,0 +1,10 @@
+def multistepsub():
+    num1 = int(input("Enter first number: "))
+    num2 = int(input("Enter second number: "))
+    num3 = int(input("Enter third number: "))
+
+    result = num1 - num2 - num3
+
+    print("Final Result:", result)
+
+multistepsub()
