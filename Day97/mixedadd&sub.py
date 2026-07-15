b243a17c37e4fca9d299fa32d498fc0ef05516b6
Ritesh Bhosale <riteshbhosale2071@gmail.com>
2026-07-15 19:35:55 +0530

    add mixedadd&sub

---
+++ Day97/mixedadd&sub.py
@@ -0,0 +1,10 @@
+def mixedoperations():
+    num1 = int(input("Enter first number: "))
+    num2 = int(input("Enter second number: "))
+    num3 = int(input("Enter third number: "))
+
+    result = num1 + num2 - num3
+
+    print("Result:", result)
+
+mixedoperations()
