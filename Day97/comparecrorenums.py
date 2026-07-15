e3722d0416cb32704de9a6bf1d9135fc07bf41f3
Ritesh Bhosale <riteshbhosale2071@gmail.com>
2026-07-15 19:21:23 +0530

    add comparecrorenums

---
+++ Day97/comparecrorenums.py
@@ -0,0 +1,12 @@
+def comparecrore():
+    num1 = int(input("Enter first number: "))
+    num2 = int(input("Enter second number: "))
+
+    if num1 > num2:
+        print("First number is greater.")
+    elif num2 > num1:
+        print("Second number is greater.")
+    else:
+        print("Both numbers are equal.")
+
+comparecrore()
