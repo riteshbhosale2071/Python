555eb97e87f4a21e1d7889f239facfc4e91641b4
Ritesh Bhosale <riteshbhosale2071@gmail.com>
2026-07-15 19:38:22 +0530

    add largenumcalculator

---
+++ Day97/largenumcalculator.py
@@ -0,0 +1,9 @@
+def largenumber():
+    num1 = int(input("Enter first large number: "))
+    num2 = int(input("Enter second large number: "))
+
+    print("Addition:", num1 + num2)
+    print("Subtraction:", num1 - num2)
+    print("Multiplication:", num1 * num2)
+
+largenumber()
