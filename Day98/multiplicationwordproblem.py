23c9f6b4e2307627e59129f812bb48d460309a9b
Ritesh Bhosale <riteshbhosale2071@gmail.com>
2026-07-16 19:17:20 +0530

    add multiplicationwordproblem

---
+++ Day98/multiplicationwordproblem.py
@@ -0,0 +1,12 @@
+def wordproblem():
+    num1 = int(input("Enter first number: "))
+    num2 = int(input("Enter second number: "))
+
+    print("Word Problem:")
+    print("A box has", num1, "apples.")
+    print("There are", num2, "boxes.")
+    print("How many apples are there in total?")
+
+    print("Answer =", num1 * num2)
+    
+wordproblem()
