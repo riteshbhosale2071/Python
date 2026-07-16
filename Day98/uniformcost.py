cd267d188bdeffcb18be1417d9cbfb8a05c3ec35
Ritesh Bhosale <riteshbhosale2071@gmail.com>
2026-07-16 19:20:57 +0530

    add uniformcost

---
+++ Day98/uniformcost.py
@@ -0,0 +1,9 @@
+def uniformcost():
+    cost = float(input("Enter cost of one uniform: "))
+    quantity = int(input("Enter number of uniforms: "))
+
+    total = cost * quantity
+
+    print("Total Uniform Cost =", total)
+
+uniformcost()
