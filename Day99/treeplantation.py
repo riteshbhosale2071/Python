04709506b7acbd4456ce88f5a6595d3a67ad8e0c
Ritesh Bhosale <riteshbhosale2071@gmail.com>
2026-07-17 19:12:46 +0530

    add Day99/treeplantation.py

---
+++ Day99/treeplantation.py
@@ -0,0 +1,9 @@
+def plantation():
+    rows = int(input("Enter number of rows: "))
+    trees_per_row = int(input("Enter number of trees in each row: "))
+
+    total_trees = rows * trees_per_row
+
+    print("Total Trees Planted =", total_trees)
+
+plantation()
