205e8a1cf33af57e3e4c484459d67825234c215f
Ritesh Bhosale <riteshbhosale2071@gmail.com>
2026-07-18 19:36:04 +0530

    add greatestcommonfactor

---
+++ Day100/greatestcommonfactor.py
@@ -0,0 +1,12 @@
+def findhcf():
+    num1 = int(input("Enter first number: "))
+    num2 = int(input("Enter second number: "))
+    hcf = 1
+
+    for i in range(1, min(num1, num2) + 1):
+        if num1 % i == 0 and num2 % i == 0:
+            hcf = i
+
+    print("HCF =", hcf)
+
+findhcf()
