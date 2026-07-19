f3da59250a474823bfc1b576e7c6a90d2509d93f
Ritesh Bhosale <riteshbhosale2071@gmail.com>
2026-07-19 18:47:13 +0530

    add primeorcomposite

---
+++ Day101/primeorcomposite.py
@@ -0,0 +1,18 @@
+def primecomposite():
+    number = int(input("Enter a number: "))
+
+    if number <= 1:
+        print("Neither Prime nor Composite")
+        return
+
+    count = 0
+    for i in range(1, number + 1):
+        if number % i == 0:
+            count += 1
+
+    if count == 2:
+        print(number, "is a Prime Number")
+    else:
+        print(number, "is a Composite Number")
+
+primecomposite()
