1be7b147ce1dcc2ed0e441ef79d1a84e38955091
Ritesh Bhosale <riteshbhosale2071@gmail.com>
2026-07-20 19:39:16 +0530

    add decimalmultiplication

---
+++ Day102/decimalmultiplication.py
@@ -0,0 +1,8 @@
+def multiplydecimal():
+    number = float(input("Enter a decimal number: "))
+    multiplier = int(input("Enter 10, 100 or 1000: "))
+
+    result = number * multiplier
+    print("Result =", result)
+
+multiplydecimal()
