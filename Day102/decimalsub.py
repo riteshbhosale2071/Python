a9a093938a9995d8e803a8e2504830db635be4a6
Ritesh Bhosale <riteshbhosale2071@gmail.com>
2026-07-20 19:34:36 +0530

    add decimalsub

---
+++ Day102/decimalsub.py
@@ -0,0 +1,8 @@
+def decimalsubtraction():
+    num1 = float(input("Enter first decimal number: "))
+    num2 = float(input("Enter second decimal number: "))
+
+    difference = num1 - num2
+    print("Difference =", difference)
+
+decimalsubtraction()
