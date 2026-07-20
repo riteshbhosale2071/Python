8f9943516166f13edd33b1bdcbc97e167e63b4ed
Ritesh Bhosale <riteshbhosale2071@gmail.com>
2026-07-20 19:41:33 +0530

    add decimaldiv

---
+++ Day102/decimaldiv.py
@@ -0,0 +1,8 @@
+def dividedecimal():
+    number = float(input("Enter a decimal number: "))
+    divisor = int(input("Enter 10, 100 or 1000: "))
+
+    result = number / divisor
+    print("Result =", result)
+
+dividedecimal()
