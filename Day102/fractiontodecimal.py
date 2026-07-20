f2b911ba535a0a0d86525b18934946130ebbb0e3
Ritesh Bhosale <riteshbhosale2071@gmail.com>
2026-07-20 19:24:41 +0530

    add fractiontodecimal

---
+++ Day102/fractiontodecimal.py
@@ -0,0 +1,8 @@
+def fractiontodecimal():
+    num = int(input("Enter numerator: "))
+    den = int(input("Enter denominator: "))
+
+    decimal = num / den
+    print("Decimal =", decimal)
+
+fractiontodecimal()
