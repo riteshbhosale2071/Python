c584db766fc6bed174b7f6d7fee6f8444edee58a
Ritesh Bhosale <riteshbhosale2071@gmail.com>
2026-07-20 19:32:20 +0530

    add decimaladd

---
+++ Day102/decimaladd.py
@@ -0,0 +1,8 @@
+def decimaladdition():
+    num1 = float(input("Enter first decimal number: "))
+    num2 = float(input("Enter second decimal number: "))
+
+    total = num1 + num2
+    print("Sum =", total)
+
+decimaladdition()
