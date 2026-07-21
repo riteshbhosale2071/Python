2b1f8bc20c5b05a123e8854d60edf6c36d7a06a1
Ritesh Bhosale <riteshbhosale2071@gmail.com>
2026-07-21 19:29:34 +0530

    add decimalquiz

---
+++ Day103/decimalquiz.py
@@ -0,0 +1,10 @@
+def decimalquiz():
+    answer = float(input("What is 2.5 + 3.4? "))
+
+    if answer == 5.9:
+        print("Correct!")
+    else:
+        print("Wrong!")
+        print("Correct Answer = 5.9")
+
+decimalquiz()
