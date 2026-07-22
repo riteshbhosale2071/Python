e9fac29ceef0c5ca73d31aa76973c4158c7c7681
Ritesh Bhosale <riteshbhosale2071@gmail.com>
2026-07-22 19:06:24 +0530

    add timescheduleplanner

---
+++ Day104/timescheduleplanner.py
@@ -0,0 +1,10 @@
+def timeschedule():
+    activity = input("Enter activity: ")
+    hour = int(input("Enter hour: "))
+    minute = int(input("Enter minute: "))
+
+    print("Today's Schedule")
+    print("Activity :", activity)
+    print("Time :", hour, ":", minute)
+
+timeschedule()
