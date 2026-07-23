420239a01639f5f07b861d8e6def4c4c45d20f1b
Ritesh Bhosale <riteshbhosale2071@gmail.com>
2026-07-23 19:08:02 +0530

    add shoppingweight

---
+++ Day105/shoppingweight.py
@@ -0,0 +1,8 @@
+def shoppingweight():
+    weight1 = float(input("Enter weight of first item (kg): "))
+    weight2 = float(input("Enter weight of second item (kg): "))
+    total_weight = weight1 + weight2
+
+    print("Total Weight =", total_weight, "kg")
+
+shoppingweight()
