3a202830bbecb56b5d0e184c7f1625a8a3e6c447
Ritesh Bhosale <riteshbhosale2071@gmail.com>
2026-07-23 19:17:55 +0530

    add cubenetvalidator

---
+++ Day105/cubenetvalidator.py
@@ -0,0 +1,9 @@
+def cubenetvalidator():
+    squares = int(input("Enter number of square faces: "))
+
+    if squares == 6:
+        print("Valid Cube Net")
+    else:
+        print("Invalid Cube Net")
+
+cubenetvalidator()
