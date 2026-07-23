77f1d49f44314f560e6bbde1af65848c36c3f611
Ritesh Bhosale <riteshbhosale2071@gmail.com>
2026-07-23 19:02:45 +0530

    add largemeasurement

---
+++ Day105/largemeasurement.py
@@ -0,0 +1,7 @@
+def largemeasurements():
+    meters = float(input("Enter measurement in meters: "))
+    kilometers = meters / 1000
+    
+    print("Kilometers =", kilometers)
+
+largemeasurements()
