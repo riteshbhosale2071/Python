40f664d38f4b1f42aa9e830c0daa022f3436ca1d
Ritesh Bhosale <riteshbhosale2071@gmail.com>
2026-07-23 19:00:19 +0530

    add measurementestimation

---
+++ Day105/measurementestimation.py
@@ -0,0 +1,10 @@
+def measurementquiz():
+    answer = float(input("How many centimeters are there in 1 meter? "))
+
+    if answer == 100:
+        print("Correct!")
+    else:
+        print("Wrong!")
+        print("Correct Answer = 100 cm")
+
+measurementquiz()
