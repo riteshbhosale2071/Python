3b5046bdbf278e72ac3d05a76683d5b6e5a8018d
Ritesh Bhosale <riteshbhosale2071@gmail.com>
2026-07-23 19:20:54 +0530

    add cuboidnetvalidator

---
+++ Day105/cuboidnetvalidator.py
@@ -0,0 +1,9 @@
+def cuboid():
+    rectangles = int(input("Enter number of rectangular faces: "))
+
+    if rectangles == 6:
+        print("Valid Cuboid Net")
+    else:
+        print("Invalid Cuboid Net")
+
+cuboid()
