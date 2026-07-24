a0d60ad8b714530a196efca2550e7179e4a94bb1
Ritesh Bhosale <riteshbhosale2071@gmail.com>
2026-07-24 19:27:29 +0530

    add rulediscoverypattern

---
+++ Day106/rulediscoverypattern.py
@@ -0,0 +1,10 @@
+def rulediscovery():
+    print("Pattern: 2 4 6 8 ?")
+    answer = int(input("Enter the next number: "))
+
+    if answer == 10:
+        print("Correct!")
+    else:
+        print("Wrong! Correct Answer is 10")
+
+rulediscovery()
