6eb26cf00c9a7acca483f73b2421dac5832d0ed9
Ritesh Bhosale <riteshbhosale2071@gmail.com>
2026-07-25 19:27:09 +0530

    add boxdivision

---
+++ Day107/boxdivision.py
@@ -0,0 +1,9 @@
+def boxdivision():
+    divisor = int(input("Enter divisor: "))
+    quotient = int(input("Enter quotient: "))
+
+    box = divisor * quotient
+
+    print("Value of □ =", box)
+
+boxdivision()
