0ae3edd01d096a61fc6429fc14e53de308e3f4ea
Ritesh Bhosale <riteshbhosale2071@gmail.com>
2026-07-25 19:37:10 +0530

    add unknownnumpuzzle

---
+++ Day107/unknownnumpuzzle.py
@@ -0,0 +1,12 @@
+def unknownnumpuzzle():
+    number = int(input("Enter the unknown number: "))
+
+    print("Puzzle: □ + 8 = 20")
+
+    if number == 12:
+        print("Correct!")
+    else:
+        print("Wrong!")
+        print("Correct Answer = 12")
+
+unknownnumpuzzle()
