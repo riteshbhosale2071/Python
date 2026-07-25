9f493200b021653738d684f7dd09817f6716fc5b
Ritesh Bhosale <riteshbhosale2071@gmail.com>
2026-07-25 19:32:17 +0530

    add algebrabalance

---
+++ Day107/algebrabalance.py
@@ -0,0 +1,10 @@
+def algebrabalance():
+    left = int(input("Enter left side value: "))
+    right = int(input("Enter right side value: "))
+
+    if left == right:
+        print("Equation is Balanced")
+    else:
+        print("Equation is Not Balanced")
+
+algebrabalance()
