fd6c673515938944418a7d2f693b374e38036705
Ritesh Bhosale <riteshbhosale2071@gmail.com>
2026-07-25 19:21:55 +0530

    add boxsubtraction

---
+++ Day107/boxsubtraction.py
@@ -0,0 +1,9 @@
+def boxsubtraction():
+    first = int(input("Enter first number: "))
+    difference = int(input("Enter difference: "))
+
+    box = first - difference
+
+    print("Value of □ =", box)
+
+boxsubtraction()
