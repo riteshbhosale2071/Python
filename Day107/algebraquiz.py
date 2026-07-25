0735fe9371fac5808ac26e9387da3462cdb2691a
Ritesh Bhosale <riteshbhosale2071@gmail.com>
2026-07-25 19:39:30 +0530

    add algebraquiz

---
+++ Day107/algebraquiz.py
@@ -0,0 +1,10 @@
+def algebraquiz():
+    answer = int(input("Solve: x + 7 = 15\nEnter value of x: "))
+
+    if answer == 8:
+        print("Correct!")
+    else:
+        print("Wrong!")
+        print("Correct Answer = 8")
+
+algebraquiz()
