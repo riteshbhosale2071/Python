6f118fe865803f872766521ee8a89712987bfa7a
Ritesh Bhosale <riteshbhosale2071@gmail.com>
2026-07-26 19:01:09 +0530

    add planeobject

---
+++ Day108/planeobject.py
@@ -0,0 +1,15 @@
+def planeobject():
+    sides = int(input("Enter number of sides: "))
+
+    if sides == 3:
+        print("Triangle")
+    elif sides == 4:
+        print("Quadrilateral")
+    elif sides == 5:
+        print("Pentagon")
+    elif sides == 6:
+        print("Hexagon")
+    else:
+        print("Unknown Plane Object")
+
+planeobject()
