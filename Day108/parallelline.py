208f08660012e9d7c155e631d81fa8a3fff0f158
Ritesh Bhosale <riteshbhosale2071@gmail.com>
2026-07-26 18:51:58 +0530

    add parallelline

---
+++ Day108/parallelline.py
@@ -0,0 +1,10 @@
+def parallelline():
+    slope1 = float(input("Enter slope of first line: "))
+    slope2 = float(input("Enter slope of second line: "))
+
+    if slope1 == slope2:
+        print("The lines are Parallel")
+    else:
+        print("The lines are Not Parallel")
+
+parallelline()
