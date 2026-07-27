7f8621ea53189800b40d8dd086523a52270857d9
Ritesh Bhosale <riteshbhosale2071@gmail.com>
2026-07-27 19:08:57 +0530

    add reflexangle

---
+++ Day109/reflexangle.py
@@ -0,0 +1,9 @@
+def reflexangle():
+    angle = float(input("Enter the angle: "))
+
+    if angle > 180 and angle < 360:
+        print("Reflex Angle")
+    else:
+        print("Not a Reflex Angle")
+
+reflexangle()
