a0dd653d7f46f5cbe2ab06978e8fe89f24d3d098
Ritesh Bhosale <riteshbhosale2071@gmail.com>
2026-07-27 19:06:28 +0530

    add zeroangle

---
+++ Day109/zeroangle.py
@@ -0,0 +1,9 @@
+def zeroangle():
+    angle = float(input("Enter angle in degrees: "))
+
+    if angle == 0:
+        print("It is a Zero Angle")
+    else:
+        print("It is Not a Zero Angle")
+
+zeroangle()
