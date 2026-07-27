c4ce87e6e4c99e4bc3fa397f70719129f0d94a3c
Ritesh Bhosale <riteshbhosale2071@gmail.com>
2026-07-27 19:16:09 +0530

    add anglebisector

---
+++ Day109/anglebisector.py
@@ -0,0 +1,8 @@
+def anglebisector():
+    angle = float(input("Enter the angle: "))
+    bisector = angle / 2
+
+    print("Original Angle:", angle)
+    print("Bisected Angle:", bisector)
+
+anglebisector()
