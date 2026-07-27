24e9398150e0c4fe52e451fa6d5121002b433c20
Ritesh Bhosale <riteshbhosale2071@gmail.com>
2026-07-27 19:23:17 +0530

    add anglecopy

---
+++ Day109/anglecopy.py
@@ -0,0 +1,10 @@
+def anglecopy():
+    angle1 = float(input("Enter the original angle: "))
+    angle2 = float(input("Enter the copied angle: "))
+
+    if angle1 == angle2:
+        print("Angle copied correctly")
+    else:
+        print("Angle copy is incorrect")
+
+anglecopy()
