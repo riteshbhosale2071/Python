4c413f15ac9a369fc6eb9f4cb56da89ec849bcac
Ritesh Bhosale <riteshbhosale2071@gmail.com>
2026-07-27 19:20:38 +0530

    add compassconstruction

---
+++ Day109/compassconstruction.py
@@ -0,0 +1,7 @@
+def compassconstruction():
+    radius = float(input("Enter the compass radius: "))
+
+    print("Compass Radius:", radius)
+    print("Draw an arc using the same radius.")
+
+compassconstruction()
