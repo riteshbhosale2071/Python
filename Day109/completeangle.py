f1a15e8b36e27bd3f34b5899b1addf513e83bb0f
Ritesh Bhosale <riteshbhosale2071@gmail.com>
2026-07-27 19:11:22 +0530

    add completeangle

---
+++ Day109/completeangle.py
@@ -0,0 +1,9 @@
+def completeangle():
+    angle = float(input("Enter the angle: "))
+
+    if angle == 360:
+        print("Complete Angle")
+    else:
+        print("Not a Complete Angle")
+
+completeangle()
