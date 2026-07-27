85680bf0be03df4cd8466f4b8b49006164c081c2
Ritesh Bhosale <riteshbhosale2071@gmail.com>
2026-07-27 19:18:26 +0530

    add equalangle

---
+++ Day109/equalangle.py
@@ -0,0 +1,9 @@
+def equalangle():
+    angle = float(input("Enter the angle: "))
+
+    equal_angle = angle
+
+    print("Original Angle:", angle)
+    print("Equal Angle Constructed:", equal_angle)
+
+equalangle()
