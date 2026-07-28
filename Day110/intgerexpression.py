036666408ef62c2c8a505154ddcc0fb869759d53
Ritesh Bhosale <riteshbhosale2071@gmail.com>
2026-07-28 19:30:19 +0530

    add intgerexpression

---
+++ Day110/intgerexpression.py
@@ -0,0 +1,10 @@
+def integerexpression():
+    num1 = int(input("Enter the first integer: "))
+    num2 = int(input("Enter the second integer: "))
+    num3 = int(input("Enter the third integer: "))
+
+    result = num1 + num2 - num3
+
+    print("Result:", result)
+
+integerexpression()
