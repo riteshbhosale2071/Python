1f01b4fba43e7ff23f1d0c6d82ea7603b8583102
Ritesh Bhosale <riteshbhosale2071@gmail.com>
2026-07-28 19:19:57 +0530

    add oppositeinteger

---
+++ Day110/oppositeinteger.py
@@ -0,0 +1,9 @@
+def oppositeinteger():
+    number = int(input("Enter an integer: "))
+
+    opposite = -number
+
+    print("Original Integer:", number)
+    print("Opposite Integer:", opposite)
+
+oppositeinteger()
