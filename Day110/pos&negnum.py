267537e58ec59069418866a345a2ce860127c28b
Ritesh Bhosale <riteshbhosale2071@gmail.com>
2026-07-28 19:09:17 +0530

    add pos&negnum

---
+++ Day110/pos&negnum.py
@@ -0,0 +1,11 @@
+def posnegnum():
+    number = int(input("Enter an integer: "))
+
+    if number > 0:
+        print("Positive Number")
+    elif number < 0:
+        print("Negative Number")
+    else:
+        print("Zero")
+
+posnegnum()
