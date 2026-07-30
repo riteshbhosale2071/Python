6c84aa4127f49b63b8b9ed8dacddf50f6fcd5d3c
Ritesh Bhosale <riteshbhosale2071@gmail.com>
2026-07-30 19:06:43 +0530

    add equivalentratio

---
+++ Day112/equivalentratio.py
@@ -0,0 +1,9 @@
+def equivalentratio():
+    first = int(input("Enter the first number of the ratio: "))
+    second = int(input("Enter the second number of the ratio: "))
+    multiplier = int(input("Enter the multiplier: "))
+
+    print("Original Ratio:", first, ":", second)
+    print("Equivalent Ratio:", first * multiplier, ":", second * multiplier)
+
+equivalentratio()
