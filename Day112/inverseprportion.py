614e400ffc9556c251315d738b3d1ce630b6ba93
Ritesh Bhosale <riteshbhosale2071@gmail.com>
2026-07-30 19:15:33 +0530

    add inverseproportion

---
+++ Day112/inverseprportion.py
@@ -0,0 +1,10 @@
+def inverseproportion():
+    x = float(input("Enter the first value: "))
+    y = float(input("Enter the second value: "))
+    new_x = float(input("Enter the new first value: "))
+
+    new_y = (x * y) / new_x
+
+    print("New second value:", new_y)
+
+inverseproportion()
