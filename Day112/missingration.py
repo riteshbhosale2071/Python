e0a39ba64b30643552e5887de5a7ac123cbf29f7
Ritesh Bhosale <riteshbhosale2071@gmail.com>
2026-07-30 19:09:54 +0530

    add missingratio

---
+++ Day112/missingration.py
@@ -0,0 +1,10 @@
+def missingratio():
+    a = int(input("Enter the first value of the first ratio: "))
+    b = int(input("Enter the second value of the first ratio: "))
+    c = int(input("Enter the first value of the second ratio: "))
+
+    missing = (b * c) // a
+
+    print("Missing value:", missing)
+
+-missingratio()
