de1fedb6d37c4a444bdcefd0f8191ef1c520f5b9
Ritesh Bhosale <riteshbhosale2071@gmail.com>
2026-08-04 19:24:08 +0530

    add squareproperty

---
+++ Day117/squareproperty.py
@@ -0,0 +1,12 @@
+def squareproperty():
+    side = float(input("Enter the side length of the square: "))
+
+    perimeter = 4 * side
+    area = side * side
+
+    print("It is a Square.")
+    print("Side Length:", side)
+    print("Perimeter:", perimeter)
+    print("Area:", area)
+
+squareproperty()
