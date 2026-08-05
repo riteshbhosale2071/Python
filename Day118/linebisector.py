a4558c4ddd1327fb746aee42f15f38f43e75cb6b
Ritesh Bhosale <riteshbhosale2071@gmail.com>
2026-08-05 19:35:15 +0530

    add linebisector

---
+++ Day118/linebisector.py
@@ -0,0 +1,10 @@
+def linebisector():
+    line_length = float(input("Enter the length of the line segment: "))
+
+    midpoint = line_length / 2
+
+    print("Original Line Length:", line_length)
+    print("Midpoint:", midpoint)
+    print("The line is divided into two equal parts of", midpoint, "units each.")
+
+linebisector()
