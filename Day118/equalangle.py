22b6db1b62b5fc62c3899027b36b8f38f5414167
Ritesh Bhosale <riteshbhosale2071@gmail.com>
2026-08-05 19:43:34 +0530

    add equalangle

---
+++ Day118/equalangle.py
@@ -0,0 +1,13 @@
+def equalangle():
+    angle = float(input("Enter the angle to copy (in degrees): "))
+
+    print("\nEqual Angle Construction Steps:")
+    print("1. Draw a base ray AB.")
+    print("2. Place the compass at the given angle's vertex and draw an arc.")
+    print("3. Draw an arc of the same radius from point A.")
+    print("4. Measure the distance between the arc intersections of the given angle.")
+    print("5. Transfer the same distance onto the new arc.")
+    print("6. Join point A to the marked point.")
+    print("The constructed angle is", angle, "degrees, equal to the given angle.")
+
+equalangle()
