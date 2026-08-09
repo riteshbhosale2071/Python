5829e7644577499268783e2b49c3042c9cdc1ca8
Ritesh Bhosale <riteshbhosale2071@gmail.com>
2026-08-09 18:28:14 +0530

    add integersign

---
+++ Day122/integersign.py
@@ -0,0 +1,11 @@
+def integersign():
+    number = int(input("Enter an integer: "))
+
+    if number > 0:
+        print("The number is Positive.")
+    elif number < 0:
+        print("The number is Negative.")
+    else:
+        print("The number is Zero.")
+
+integersign()
