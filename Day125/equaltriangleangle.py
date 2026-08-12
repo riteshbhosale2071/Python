de997ff90d392856ae36d6e6d9b1013cde381b45
Ritesh Bhosale <riteshbhosale2071@gmail.com>
2026-08-12 20:50:15 +0530

    add equaltriangleangle

---
+++ Day125/equaltriangleangle.py
@@ -0,0 +1,9 @@
+def equaltriangleangle():
+    print("An equilateral triangle has three equal angles.")
+
+    total_angle = 180
+    angle = total_angle / 3
+
+    print("Each Angle:", angle, "°")
+
+equaltriangleangle()
