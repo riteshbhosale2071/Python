e54db9211a58ce9cfe2d7cb7ba04a2321b857863
Ritesh Bhosale <riteshbhosale2071@gmail.com>
2026-08-18 18:51:21 +0530

    add losspercentage

---
+++ Day131/losspercentage.py
@@ -0,0 +1,20 @@
+def losspercentage():
+    cost_price = float(input("Enter cost price: "))
+    selling_price = float(input("Enter selling price: "))
+
+    if cost_price <= 0 or selling_price < 0:
+        print("Please enter valid values.")
+        return
+
+    if selling_price < cost_price:
+        loss = cost_price - selling_price
+        loss_percentage = (loss / cost_price) * 100
+
+        print("Loss:", loss)
+        print("Loss Percentage:", loss_percentage, "%")
+    elif selling_price == cost_price:
+        print("No Profit, No Loss.")
+    else:
+        print("There is a profit, not a loss.")
+
+losspercentage()
