9530639692fc86ac6a8c2003bf17d3ec34bd12c0
Ritesh Bhosale <riteshbhosale2071@gmail.com>
2026-08-24 19:07:08 +0530

    add deficientnumberchecker

---
+++ Day137/deficientnumberchecker.py
@@ -0,0 +1,19 @@
+def deficientnumberchecker():
+    number = int(input("Enter a positive integer: "))
+
+    if number <= 0:
+        print("Please enter a positive integer.")
+        return
+
+    factor_sum = 0
+
+    for i in range(1, number):
+        if number % i == 0:
+            factor_sum += i
+
+    if factor_sum < number:
+        print("The number is a Deficient Number.")
+    else:
+        print("The number is not a Deficient Number.")
+
+deficientnumberchecker()
