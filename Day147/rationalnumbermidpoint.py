f05699998cc7427ff8057aefd8909579800701a1
Ritesh Bhosale <riteshbhosale2071@gmail.com>
2026-09-03 19:37:53 +0530

    add rationalnumbermidpoint

---
+++ Day147/rationalnumbermidpoint.py
@@ -0,0 +1,25 @@
+from fractions import Fraction
+
+def rationalnumbermidpoint():
+    n1 = int(input("Enter numerator of first rational number: "))
+    d1 = int(input("Enter denominator of first rational number: "))
+
+    n2 = int(input("Enter numerator of second rational number: "))
+    d2 = int(input("Enter denominator of second rational number: "))
+
+    if d1 == 0 or d2 == 0:
+        print("Denominator cannot be zero.")
+        return
+
+    number1 = Fraction(n1, d1)
+    number2 = Fraction(n2, d2)
+
+    midpoint = (number1 + number2) / 2
+
+    print("\nRational Number Midpoint :")
+    print("First Rational Number:", number1)
+    print("Second Rational Number:", number2)
+    print("Midpoint:", midpoint)
+    print("Decimal Value:", float(midpoint))
+
+rationalnumbermidpoint()
